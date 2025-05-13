0cfb33673640cb60ec247c4b998470aa74135788
karufeliciano <kareenfeliciano@gmail.com>
2025-05-13 02:13:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let paragrafo= document querySelector ("#paragrafo")
+
+paragrafo.addEventListener ("mouseover", Troca texto);
+
+function Troca texto (){
+    paragrafo.style.background= "green"; 
+}
